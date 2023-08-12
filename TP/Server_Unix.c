f48f242148ffe230df6a7c34676146dfdb947c9b
briascoleonel <leonelbriasco@mi.unc.edu.ar>
2023-08-11 21:21:25 -0300

    Se agregan el manejo de argumento del handler Se agrega la funcion ocupar handler Se gestiona la salida del servidor esperando a que terminen todos los hilos que estan ejecutandose Se cierran los fd y se libera memoria Falta comentar

--- TP/Server_Unix.c
+++ TP/Server_Unix.c
@@ -23,6 +23,7 @@ void server_conf_socket_Unix(int *sock, struct sockaddr_un *serv_addr, long unsi
 void* file_writing_thread(void * arg);
 int get_cant_hand_disp(int *Handlers, long unsigned int maxHandlers);
 int get_prim_hand_disp(int *Handlers, long unsigned int maxHandlers);
+void ocupar_handler(int *Handlers, int i, pthread_mutex_t *lock);
 
 //Struct que vamos a utilizar
 
@@ -30,15 +31,18 @@ struct UNIX_arg_struct {
     char UNIX_File_Name[MAXLINE];
     char UNIX_Write_File_Name[MAXLINE];
     pthread_mutex_t *global_lock;
-    int maxClientes;
+    int max_clientes;
     int *salir;
 };
 
 struct local_threads_arg_struct {
     int id;
     int *ConnSocket;
+    int ExitThread;
+    int *Handlers;
     pthread_mutex_t *lock;
     pthread_mutex_t *global_lock;
+    int *salir;
 };
 
 struct local_writer_arg_struct {
@@ -56,16 +60,16 @@ void *Unix_Server(void *arg)
     int *connfd;        //FD para el socket una vez ocurra el accept
     struct sockaddr_un serv_addr;       //Struct para pasar direccion
 
-    connfd = malloc((unsigned long int) argumentos->maxClientes * sizeof(int));
+    connfd = malloc((unsigned long int) argumentos->max_clientes * sizeof(int));
 
     //Creamos los hilo y mutex para que no haya acceso al mismo tiempo
     pthread_t *task_thread;
-    struct local_thread_arg_struct *handler_thread_args;
+    struct local_threads_arg_struct *handler_thread_args;
     pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
     //Asignacion de memoria para el thread utilizando cantidad maxima de clientes como referencia
-    task_thread = malloc((unsigned long int) argumentos->maxClientes *sizeof(pthread_t));
-    handler_thread_args = malloc((unsigned long int) argumentos->maxClientes * sizeof(struct local_threads_arg_struct));
+    task_thread = malloc((unsigned long int) argumentos->max_clientes *sizeof(pthread_t));
+    handler_thread_args = malloc((unsigned long int) argumentos->max_clientes * sizeof(struct local_threads_arg_struct));
 
     //Inicializamos handlers para manejar los threads
     long unsigned int cant_handlers_disp = 0;
@@ -73,7 +77,7 @@ void *Unix_Server(void *arg)
     int sig_handler;            //Utiliza el primer handler que se encuentra
 
     //Asignacion de memoria
-    handlers_disp = malloc((unsigned long int)argumentos->maxClientes *sizeof(int));
+    handlers_disp = malloc((unsigned long int)argumentos->max_clientes *sizeof(int));
 
     //Hilo para excribir archivo con bytes recibidos
     pthread_t file_writer_thread;
@@ -84,10 +88,10 @@ void *Unix_Server(void *arg)
     file_writer_arg.salir = &argumentos->salir;
 
     //Para configurar el socket se utiliza esta funcion
-    server_conf_socket_Unix(&listenfd, &serv_addr, (unsigned long int)argumentos->maxClientes, argumentos->UNIX_File_Name);
+    server_conf_socket_Unix(&listenfd, &serv_addr, (unsigned long int)argumentos->max_clientes, argumentos->UNIX_File_Name);
 
     //Iniciamos todos los handlers como disponibles
-    for(int i=0; i<argumentos->maxClientes; i++)
+    for(int i=0; i<argumentos->max_clientes; i++)
     {
         handlers_disp[i] = 1;
     }
@@ -102,17 +106,17 @@ void *Unix_Server(void *arg)
         //En exclusion mutua usando el lock
         pthread_mutex_lock(&lock);
         //Usa la funcion get_cant_hand_disp
-        cant_handlers_disp = (long unsigned int) get_cant_hand_disp(handlers_disp,(unsigned long int) argumentos->maxClientes);
+        cant_handlers_disp = (long unsigned int) get_cant_hand_disp(handlers_disp,(unsigned long int) argumentos->max_clientes);
         pthread_mutex_unlock(&lock);    //Libero el lock
 
         //Compruebo que queden handlers disponibles
         //Usa get_prim_hand_disp para comprobar tambien que haya disponibles
-        if(cant_handlers_disp == 0 || get_prim_hand_disp(handlers_disp, (unsigned long int)argumentos->maxClientes) < 0)
+        if(cant_handlers_disp == 0 || get_prim_hand_disp(handlers_disp, (unsigned long int)argumentos->max_clientes) < 0)
         {
             while(cant_handlers_disp == 0)
             {
                 pthread_mutex_lock(&lock);
-                cant_handlers_disp = (long unsigned int)getAvailableHandlersAmount(handlers_disp, (unsigned long int)argumentos->maxClientes);
+                cant_handlers_disp = (long unsigned int)getAvailableHandlersAmount(handlers_disp, (unsigned long int)argumentos->max_clientes);
                 pthread_mutex_unlock(&lock);
             }
         }
@@ -120,7 +124,7 @@ void *Unix_Server(void *arg)
         //Pasando la prueba de disponibilidad, agarra el primero disponible
         //Tambien en exclusion mutua
         pthread_mutex_lock(&lock);
-        sig_handler = get_cant_hand_disp(handlers_disp, (unsigned long int)argumentos->maxClientes);
+        sig_handler = get_cant_hand_disp(handlers_disp, (unsigned long int)argumentos->max_clientes);
         pthread_mutex_unlock(&lock);
 
         fflush(stdout);
@@ -134,7 +138,53 @@ void *Unix_Server(void *arg)
             printf("Error en accept:\n");
             exit(EXIT_FAILURE);
         }
+
+        handler_thread_args[sig_handler].id = sig_handler;
+        handler_thread_args[sig_handler].ConnSocket = &(connfd[sig_handler]);
+        handler_thread_args[sig_handler].ExitThread = 0;
+        handler_thread_args[sig_handler].Handlers = handlers_disp;
+        handler_thread_args[sig_handler].lock = &lock;
+        handler_thread_args[sig_handler].global_lock = argumentos->global_lock;
+        handler_thread_args[sig_handler].salir = argumentos->salir;
+
+        ocupar_handler(handlers_disp,sig_handler,&lock);
+
     }
+
+    if(*(argumentos->salir) == 1)
+    {
+        for(int i=0; i<argumentos->max_clientes;i++)
+        {
+            pthread_mutex_lock(&lock);
+            if(handlers_disp[i] == 1)
+            {
+                pthread_join(task_thread[i],NULL);
+            }
+            pthread_mutex_unlock(&lock);
+        }
+    }
+
+    for(int i=0;i<argumentos->max_clientes;i++)
+    {
+        if((close(connfd[i])<0))
+        {
+            printf("Erro al cerrar conn %d\n",i);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    if((close(listenfd)<0)) 
+    {
+        printf("Error al cerrar listenfd\n");
+        exit(EXIT_FAILURE); 
+    }
+
+    free(handlers_disp);   
+    free(task_thread);          
+    free(connfd);              
+    free(handler_thread_args); 
+
+    return NULL;
 
 }
 
@@ -208,4 +258,11 @@ int get_prim_hand_disp(int *Handlers, long unsigned int maxHandlers)
         }
     }
     return -1;
+}
+
+void ocupar_handler(int *Handlers, int i, pthread_mutex_t *lock)
+{
+    pthread_mutex_lock(lock);
+    Handlers[i] = 0;
+    pthread_mutex_unlock(lock);
 }
